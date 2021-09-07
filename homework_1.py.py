a5fda8147a21ecb53b47d1768911e011df8f377a
Dmitry Bublin <3691624@gmail.com>
2021-09-07 22:55:57 +0300

    переменные

---
+++ homework_1.py.py
@@ -0,0 +1,66 @@
+# 1) создать переменную типа string
+first_name = "Dmitry"
+print(first_name, type(first_name))
+# 2) создать переменную типа Integer
+age = 40
+print(age, type(age))
+# 3) создать переменную типа Float
+weight = 81.5
+print(weight, type(weight))
+# 4) создать переменную типа Bytes
+first_bytes = b"python"
+print(first_bytes, type(first_bytes))
+# 5) создать переменную типа list
+my_list = [1, 2, 3]
+print(my_list, type(my_list))
+# 6) создать переменную типа Tuple
+my_tuple = (1, 2, 3)
+print(my_tuple, type(my_tuple))
+# 7) создать переменную типа Set
+my_set = {1, 2, 3}
+print(my_set, type(my_set))
+# 8) создать переменную типа Frozenset
+my_frozenset = ([1, 2, 3])
+print(my_frozenset, type(my_frozenset))
+# 9) создать переменную типа dict
+first_dict = {"name": " Dmitry "}
+print(first_dict, type(first_dict))
+
+#11) Создать 2 переменные String, создать переменную в которой суммируете эти переменные. Вывести в консоль.
+
+a = "avia"
+b = "show"
+look = (a + b)
+print(look)
+
+#12) Создать 2 переменные Integer, создать переменную в которой суммируете эти переменные. Вывести в консоль.
+
+salary = 500
+advance = 200
+total = (salary + advance)
+print(total)
+
+# 13) Создать переменную в которой Разделите эти переменные Int. Вывести в консоль.
+
+X_X = (salary / advance)
+print(X_X)
+
+# 14) Создать переменную в которой Умножите эти переменные Int. Вывести в консоль.
+
+small = (salary * advance)
+print(small)
+
+# 15) Создать переменную в которой Разделите без остатка эти переменные Int. Вывести в консоль.
+
+Big = (salary // advance)
+print(Big)
+
+# 16) Создать переменную в которой надо присвоить остаток от деления этих переменные Int. Вывести в консоль.
+
+bez_ost = (salary % advance)
+print(bez_ost)
+
+
+
+
+
